85baf2de85687bd261da8fb69440091a66f1beeb
Ea-Bell <cjp975@naver.com>
2021-05-12 04:46:22 +0000

    test

---
+++ module/settting/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	server_port :8080,
+	db_durl:'mongodb+srv://EaBell:ggKy7EjgOjrGaN5S@cluster0.plbij.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+	
+	db_schemas:[
+		{file:'./user_shema', collection:''}
+	]
+}
