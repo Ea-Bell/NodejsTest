d878ae46310c67455733687a745b31950b6d3c2f
Ea-Bell <cjp975@naver.com>
2021-05-18 09:37:16 +0000

    핸드폰에서 커밋 테스트중 페이지네이션도 추가 커밋

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const methodOverride = require('method-override')
 const MongoClient = require('mongodb').MongoClient
 const router = require('express').Router();
+const mongoose = require('mongoose');
 app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'ejs');
 app.use('/public', express.static('public'))
@@ -12,7 +13,6 @@ app.use(express.json());
 
 const port = 8080;
 const uri = 'mongodb+srv://EaBell:7hSV2A1o9LI1YizP@cluster0.plbij.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-
 
 var db;
 var dbconut;
@@ -39,6 +39,45 @@ app.get('/list/:id', function (req, res) {
         res.render('list.ejs', { posts: result })
     });
 });
+//페이지 보여줌
+app.get('/list2/:id', function (req, res) {
+	let a="test"
+	//컨텐츠 보여줄때
+	//페이지네이션은 조건이 3가지이다 
+	//1번째 1보다 작은경우 d<1 
+
+	
+	//2번째 사잇값  1<d<max
+	//3번째 max값 
+    dbPost.find().toArray(function (err, result) {
+        // console.log(listPage 요청);
+	let currentPage =req.params.id; //현재 위치	
+	let contentSize= result[currentPage];
+	let maxSize= result.length;  // maxSize
+		
+		if(currentPage<1){		
+						res.redirect('/list2/1');
+						
+			}
+		else if(1<=currentPage&&currentPage<=maxSize){
+			// //변수 2개? 
+			// for(i=contentSize;i<contentSize*5;i++){
+			// 	console.dir("i", contentSize);
+			// }
+			console.log("asdfeeee:"+currentPage);
+			console.log("maxSize:"+typeof maxSize.toString());
+			res.render('list2.ejs',{posts:result,test:a, contentSize=contentSize})
+		}else if(currentPage > maxSize){
+			console.log("maxSize찍음")
+			res.redirect('/list2/'+maxSize);
+		}else{	
+	console.dir("현재 컨텐츠의 max: "+result.length);
+	}
+	
+        // res.render('list2.ejs', { posts: result, test:a })
+    });
+});
+
 app.get('/test', function (req, res) {
     res.render('test.ejs');
 })
@@ -69,14 +108,12 @@ app.post('/add', function (req, res) {
 
     dbconut.findOne({ name: '게시물갯수' }, function (err, result) {
         if (err) return console.log(err);
-
+		
         console.dir("게시물 갯수 result:" + result);
         noticeBoard = result.totalPost;
         res.send("전송완료");
 
-
         //익스프레스 객체 생성
-
         console.log("query: " + req.query.name + "," + req.query.name);
         console.log("body: " + req.body.score + "," + req.body.score);
 
@@ -91,9 +128,7 @@ app.post('/add', function (req, res) {
                 if (err) {
                     return console.log(err);
                 }
-
             });
-
             console.log('insert 완료!');
             dbconut.updateOne
         });
